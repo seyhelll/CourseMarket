45566488d05613af90cea8be1f0fee9c38c37533
seyhelll <nataliukhinova@gmail.com>
2026-04-24 21:00:18 +0300

    Update PlayStoreApp.swift

--- PlayStore/PlayStoreApp.swift
+++ PlayStore/PlayStoreApp.swift
@@ -1,36 +1,10 @@
-//
-//  PlayStoreApp.swift
-//  PlayStore
-//
-//  Created by Александр Бисеров on 22.04.2025.
-//
-
 import SwiftUI
-import SwiftData
 
 @main
-struct PlayStoreApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-            User.self,
-            Product.self,
-            Cart.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+struct CourseMarketApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
-        .modelContext(sharedModelContainer.mainContext)
     }
 }
